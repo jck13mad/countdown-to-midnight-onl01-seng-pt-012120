e84118e95084bd5e0beb1c79d5c98f9103e794e9
Jack Madden <jck13mad@gmail.com>
2020-01-24 14:57:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
-num = 10
 
 def countdown(num)
   while num != 0
-    return "#{num} SECOND(S)!"
+    puts "#{num} SECOND(S)!"
     num -= 1
-    sleep(1)
+    sleep(1.SECOND)
   end
-  return "HAPPY NEW YEAR!"
+  puts "HAPPY NEW YEAR!"
 end
